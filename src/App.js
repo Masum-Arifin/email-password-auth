b1d0132aa6f370462267f27abd09d860d5d2748b
Masum-Arifin <masumarifinbd@gmail.com>
2022-04-08 23:23:16 +0600

    new code

--- src/App.js
+++ src/App.js
@@ -1,47 +1,115 @@
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import app from "./firebase.init";
+import { useState } from "react";
 
 const auth = getAuth(app);
 function App() {
+  const [validated, setValidated] = useState(false);
+  const [error, setError] = useState('')
+  const [registered, setRegistered] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleEmailBlur = (e) => {
-    console.log(e.target.value);
+    setEmail(e.target.value);
   };
 
   const handlePasswordBlur = (e) => {
-    console.log(e.target.value);
+    setPassword(e.target.value);
   };
 
+  const handleRegisteredChange = e=>{
+    setRegistered(e.target.checked);
+  }
+
   const handleFormSubmit = (e) => {
-    console.log("form submitted");
     e.preventDefault();
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.stopPropagation();
+      return;
+    }
+
+    if (!/(?=.*?[#?!@$%^&*-])/.test(password)) {
+      setError(' Please should contain at least one special character')
+    }
+
+    setValidated(true);
+    setError('')
+
+    if(registered){
+      signInWithEmailAndPassword(auth, email, password)
+      .then(result =>{
+        const user = result.user;
+        console.log(user);
+      })
+      .catch(error =>{
+        console.error(error);
+        setError(error.message)
+      })
+    }
+    else{
+      createUserWithEmailAndPassword(auth, email, password)
+      .then((results) => {
+        const user = results.user;
+        console.log(user);
+        setEmail("");
+        setPassword("");
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error.message)
+      });
+    }
+
+ 
+    e.preventDefault(); 
   };
 
   return (
-    <div >
-      <div className="registration w-50 mx-auto mt-3">
-        <h2 className="text-primary">Please Register</h2>
-        <Form onSubmit={handleFormSubmit}>
+    <div>
+      <div className="registration w-50 mx-auto mt-5">
+        <h2 className="text-primary">Please {registered ? 'Login': 'Register'}</h2>
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+            <Form.Control
+              onBlur={handleEmailBlur}
+              type="email"
+              placeholder="Enter email"
+              required
+            />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid email.
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              onBlur={handlePasswordBlur}
+              type="password"
+              placeholder="Password"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid password.
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
+            <Form.Check noChange={handleRegisteredChange} type='checkbox' label='Already Registered?'/>
           </Form.Group>
+          <p className="text-danger">{error}</p>
           <Button variant="primary" type="submit">
-            Submit
+            {registered? 'Login': 'Register'}
           </Button>
         </Form>
       </div>
